e197c4121715976f53f4bd4d428ebe53f5d8ec33
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-05-02 08:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food , breed) {
+    this.food  = food ;
+    this.drink = drink;
+  }
+}
+
+let Breakfast1 = new Breakfast("shfta", "water");
+ Breakfast1;
+ 
+ class Lunch {
+    constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+let Lunch1 = new Lunch("shfta", "chikensoap", "water");
+ Lunch1;
+
+class Dinner {
+    constructor(salad, soup, entree, _dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
+let Dinner1 = new Dinner("shfta", "chikensoap", "idk", "paqlawa");
+ Dinner1;
